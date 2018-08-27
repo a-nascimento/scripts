6dc6673531b1f242a7e8f1bbf7e8bbf178959054
Andrew Nascimento <andrew_nascimento@surveysampling.com>
2018-08-27 10:31:29 -0400

    changes to vmware py

--- python/vmware/vmware.py
+++ python/vmware/vmware.py
@@ -1,19 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 from requests import Request, Session
-import urllib3
 
 def python_version_check():
-    #python3 import urllib3
-    #python2 import ssl 
-    print('hello')
+    import platform
 
+    version = platform.python_version().split('.')[0]
+    try:
+        if  version == '2':
+            print('version 2')
+            import requests
+            from requests.packages.urllib3.exceptions import InsecureRequestWarning
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+            request_get()
+        elif version == '3':
+            print('version 3')
+            import urllib3
+            urllib3.disable_warnings()
+            request_get()
+        else:
+            raise ValueError        
+    except ValueError:
+        print('Not a valid version')
+        
 def request_get():
-    urllib3.disable_warnings()
+    global
+    print('in request_get')
+    #urllib3.disable_warnings()
     url = 'https://ctvcsa.surveysampling.com/rest/vcenter/network/'
+    print(url)
     s = Session()
     #s.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
     # #s.auth = ('surveysampling\andrew_nascimento', 'pass')
-    response = s.get(url, verify=False, auth=('surveysampling\andrew_nascimento','pass'))
-    response
+    
+    requests.get(url, verify=False)
+    
 
+# Maint Script
+python_version_check()
